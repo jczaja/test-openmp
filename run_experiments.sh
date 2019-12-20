37b1cbacb04f310e51b465ad284e2151b1a68005
Jacek Czaja <jacek.czaja@intel.com>
2019-12-20 13:34:29 +0100

    - Fix to experiments running script

--- run_experiments.sh
+++ run_experiments.sh
@@ -12,7 +12,7 @@ else
   pushd build-conv-nchw
   cmake ../ -DCMAKE_BUILD_TYPE=Release -DALGO=dnnl_nchw_conv -DN=256 -DC=3 -DW=227 -DH=227 -DNF=96 -DHF=11 -DWF=11
   sudo make enable_turbo_boost
-  make -j traffic
+  make -j test-openmp-gomp
   sudo make disable_turbo_boost
   make roofline
   cp roofline* cputest.txt memtest.txt runtime.txt traffic.txt work.txt algo_info.txt cpu_info.txt $DATA_DIR/dnnl_nchw_conv/
@@ -24,7 +24,7 @@ else
   pushd build-conv-blocked
   cmake ../ -DCMAKE_BUILD_TYPE=Release -DALGO=dnnl_blocked_conv -DN=256 -DC=3 -DW=227 -DH=227 -DNF=96 -DHF=11 -DWF=11
   sudo make enable_turbo_boost
-  make -j traffic
+  make -j test-openmp-gomp
   sudo make disable_turbo_boost
   make roofline
   cp roofline* cputest.txt memtest.txt runtime.txt traffic.txt work.txt algo_info.txt cpu_info.txt $DATA_DIR/dnnl_blocked_conv/
@@ -36,7 +36,7 @@ else
   pushd build-tnc-layer_norm
   cmake ../ -DCMAKE_BUILD_TYPE=Release -DALGO=dnnl_tnc_layer_norm -DN=256 -DC=768 -DW=4 -DH=32
   sudo make enable_turbo_boost
-  make -j traffic
+  make -j test-openmp-gomp
   sudo make disable_turbo_boost
   make roofline
   cp roofline* cputest.txt memtest.txt runtime.txt traffic.txt work.txt algo_info.txt cpu_info.txt $DATA_DIR/dnnl_tnc_layer_norm/
